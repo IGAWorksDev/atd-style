488fed29277ed7cb3eb7a6c7aff4789f9595269b
zena-igaw <zena@igaworks.com>
2021-01-14 14:31:26 +0900

    mainheader 스크롤링 고정

--- assets/js/tw-atd-normal.js
+++ assets/js/tw-atd-normal.js
@@ -1,6 +1,25 @@
-
-
 $(function () {
+
+    // Header bar function
+    $.scrollWindow = function () {
+        if ($(window).scrollTop() > 56) {
+            $('.mainheader-sticky').addClass('is-fixed');
+        } else {
+            $('.mainheader-sticky').removeClass('is-fixed');
+        };
+    };
+    $.scrollWindow();
+    $(window).scroll(function () {
+        $.scrollWindow();
+    });
+    $(window).resize(function () {
+        $.scrollWindow();
+    });
+
+
+    // Tooltip
     $('[data-toggle="tooltip"]').tooltip();
+
+    // Dropdown
     $('.dropdown-toggle').dropdown();
 })
